666fe0fe0025ceb21ab1cb1cc98bd058562ecb8e
Mogs <olajawoonhakwob@gmail.com>
2025-06-11 10:33:01 +0800

    Fix Tailwind CSS build - downgrade to v3 and update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./admin.html",
+    "./admin.tsx",
+    "./AdminApp.tsx",
+    "./App.tsx",
   ],
   darkMode: 'class',
   theme: {
     extend: {
       // Custom theme extensions can be added here
+      maxWidth: {
+        '7xl': '80rem',
+      }
     },
   },
   plugins: [],
